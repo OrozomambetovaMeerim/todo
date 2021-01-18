04effcf90b2f3efabcf01189af99a9cabead6cc9
OrozomambetovaMeerim <meerimorozomambetova@gmail.com>
2021-01-19 02:14:28 +0600

    created models Books

--- main/models.py
+++ main/models.py
@@ -6,3 +6,15 @@ class ToDo(models.Model):
     created_at = models.DateField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+
+class Books(models.Model):
+    title = models.CharField(max_length = 100)
+    subtitle = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    price = models.IntegerField()
+    author = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    year = models.DateField()
+    created_at = models.DateField()
+
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from .models import ToDo
+from .models import ToDo, Books
 
 
 def homepage(request):
@@ -10,6 +10,10 @@ def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
 
+
+def books(request):
+    books_list = Books.objects.all()
+    return render(request, "books.html", {"books_list": books_list})
 
 def second(request):
     return HttpResponse("test 2 page")
